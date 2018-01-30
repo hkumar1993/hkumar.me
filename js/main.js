1eaec3f25217ca9749715e8f7388e43dbb4157f9
Harsh <harsh@hkumar.me>
2018-01-29 21:31:43 -0800

    Add code for toggle nav menu

--- js/main.js
+++ js/main.js
@@ -4,17 +4,21 @@ let navToggle = false;
 document.addEventListener('DOMContentLoaded', function(){
     const tripleDot = document.querySelector('.triple-dot')
     const hiddenNav = document.querySelector('.hidden-nav')
+    const darkShroud = document.querySelector('.dark-shroud')
     console.log(hiddenNav)
 
     function toggleNav() {
         console.log('clicked')
         if (navToggle) {
             hiddenNav.classList.remove('show')
+            darkShroud.classList.add('hidden')
         } else {
             hiddenNav.classList.add('show')
+            darkShroud.classList.remove('hidden')
         }
         navToggle = !navToggle
     }
 
     tripleDot.addEventListener('click', toggleNav);
+    darkShroud.addEventListener('click', toggleNav);
 })
